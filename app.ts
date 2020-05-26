9ece83baa11923918baffbfafa1b9986724ca04a
Tomas Wiklo <kaizen@Tomass-MacBook-Pro.local>
2020-05-26 08:52:39 +0200

    enum, two possible strings

--- app.ts
+++ app.ts
@@ -1,1 +1,30 @@
-console.log('auto reload');
+enum Role {
+  ADMIN, 
+  READ_ONLY,
+  AUTHOR
+}
+
+
+const person = {
+  name: 'Tomas',
+  age: 30,
+  elo: 11,
+  hobbies: ['sport', 'swim'],
+  role: Role.READ_ONLY
+}
+
+console.log(person.role);
+
+
+let favoriteActivies: any[];
+favoriteActivies = ['test', 4];
+//person.hobbies = [2];
+
+person.name = 'Wiklo';
+
+console.log(favoriteActivies);
+
+let hobby: string;
+for (hobby of person.hobbies) {
+  console.log(hobby);
+}
